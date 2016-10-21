b392d374193c0f509d5fa25a97ca7adac5a25dc8
EryouHao <haoeryou@qq.com>
2016-10-21 10:23:10 +0800

    更新 README,优化js

--- js/music.js
+++ js/music.js
@@ -1,39 +1,46 @@
+/**
+ *  作者：海岛心hey
+ *  最近更改时间：2016-10-21
+ *  个人网站：fehey.com
+ *  邮箱：haoeryou@qq.com
+ */
+
 window.onload = function () {
 
-	var audio = document.getElementById('audio'); // 音乐
-	var sourceList = audio.getElementsByTagName('source'); // 音乐列表
-	var play = document.getElementById('play'); // 播放按钮
-	var prev = document.getElementById('prev'); // 上一曲
-	var next = document.getElementById('next'); // 下一曲
-	var jingyin = document.getElementById('jingyin'); // 静音
-	var voiceBar = document.getElementById('voice-bar'); // 音量调节
-	var musicBar = document.getElementById('music-bar'); // 播放进度控制
-	var playedBar = document.getElementById('played-bar'); // 已播放进度条
-	var voicedBar = document.getElementById('voiced-bar'); // 已播放进度条
-	var musicTitle = document.getElementById('title'); // 音乐标题
-	var loadBar = document.getElementById('load-bar'); // 加载进度条
-	var currentTime = document.getElementById('current-time'); // 当前音乐时间
-	var totalTime = document.getElementById('total-time'); // 当前音乐时间
-	var musicImg = document.getElementById('music-img');
-	var xunhuan = document.getElementById('xunhuan'); // 循环按钮
-	var currentSrcIndex = 0;
+	var audio = document.getElementById('audio'), // 音乐
+	 	sourceList = audio.getElementsByTagName('source'), // 音乐列表
+	 	play = document.getElementById('play'), // 播放按钮
+	 	prev = document.getElementById('prev'), // 上一曲
+	 	next = document.getElementById('next'), // 下一曲
+	 	jingyin = document.getElementById('jingyin'), // 静音
+	 	voiceBar = document.getElementById('voice-bar'), // 音量调节
+	 	musicBar = document.getElementById('music-bar'), // 播放进度控制
+	 	playedBar = document.getElementById('played-bar'), // 已播放进度条
+	 	voicedBar = document.getElementById('voiced-bar'), // 已播放进度条
+	 	musicTitle = document.getElementById('title'), // 音乐标题
+	 	loadBar = document.getElementById('load-bar'), // 加载进度条
+	 	currentTime = document.getElementById('current-time'), // 当前音乐时间
+	 	totalTime = document.getElementById('total-time'), // 当前音乐时间
+	 	musicImg = document.getElementById('music-img'), // 音乐封面
+	 	xunhuan = document.getElementById('xunhuan'); // 循环按钮
+	 	currentSrcIndex = 0;
 
 
 	// 是否循环播放 
 	audio.loop = false;
 
-	// 自动播放
+	// 是否自动播放
 	audio.autoplay = false;
 
-	// 设置音量
+	// 初始化音量
 	audio.volume = 0.5;
 	var voicedBarWidth = (audio.volume / 1) * voiceBar.clientWidth;
 	voicedBar.style.width = voicedBarWidth + 'px';
 
-	// 缓冲加载
+	// 是否自动缓冲加载
 	audio.autobuffer = false;
 
-	// 显示时长
+	// 显示第一首歌曲时长
 	(function () {
 		var fen = parseInt(audio.duration / 60);
 		var miao = parseInt(audio.duration % 60);
@@ -50,38 +57,37 @@ window.onload = function () {
 			audio.play();
 			this.innerHTML = 'Pause';
 			musicImg.style.animation = 'xuanzhuan 3s linear infinite';
-
 		} else {
 			audio.pause();
 			this.innerHTML = 'Play';
 			musicImg.removeAttribute('style');
 		}
-
-		musicTitle.innerHTML = sourceList[currentSrcIndex].title;
 		musicTime();
 	};
 
 	// 下一曲
 	next.onclick = function () {
-		++ currentSrcIndex > sourceList.length - 1 && (currentSrcIndex = 0);
-		currentSrc = sourceList[currentSrcIndex].getAttribute('src');
-		audio.setAttribute('src', currentSrc);
-		audio.play();
-		play.innerHTML = 'Pause';
-		musicTitle.innerHTML = sourceList[currentSrcIndex].title;
-		musicTime();
+		changeMusic('next');
 	};
 
 	// 上一曲
 	prev.onclick = function () {
-		-- currentSrcIndex < 0 && (currentSrcIndex = sourceList.length -1);
+		changeMusic('prev');
+	};
+
+	// 切换歌曲
+	function changeMusic (direct) {
+		if (direct === 'next') {
+			++ currentSrcIndex > sourceList.length - 1 && (currentSrcIndex = 0); // 下一曲
+		} else {
+			-- currentSrcIndex < 0 && (currentSrcIndex = sourceList.length -1); // 上一曲
+		}
 		currentSrc = sourceList[currentSrcIndex].getAttribute('src');
 		audio.setAttribute('src', currentSrc);
 		audio.play();
 		play.innerHTML = 'Pause';
-		musicTitle.innerHTML = sourceList[currentSrcIndex].title;
 		musicTime();
-	};
+	}
 
 	// 音量调节（增加黄色现在音量显示）
 	voiceBar.onclick = function (event) {
@@ -102,14 +108,10 @@ window.onload = function () {
 		audio.currentTime = newCurrentTime;
 		var playedBarWidth = (audio.currentTime / audio.duration) * musicBarWidth;
 		playedBar.style.width = playedBarWidth + 'px';
-
-		// 增加自动下一曲功能！！！！
 	};
 
 	// 播放进度实时更新(修改为歌曲播放时开启定时器，暂停和页面load时清除定时器)
-
 	setInterval(function updatePlayedBar (){
-		console.log('执行了');
 		var musicBarWidth = musicBar.clientWidth;
 		var playedBarWidth = (audio.currentTime / audio.duration) * musicBarWidth;
 		playedBar.style.width = playedBarWidth + 'px';
@@ -118,12 +120,12 @@ window.onload = function () {
 		} else {
 			currentTime.innerHTML = parseInt(audio.currentTime / 60) + ':' + parseInt(audio.currentTime % 60);
 		}
-		//如果是时间结束，并且是非单曲循环
+		//如果是时间结束，并且是非单曲循环，自动下一曲
 		if (audio.currentTime === audio.duration && !audio.loop) {
 			next.onclick();
 		}
 	}, 1000);
-
+	
 	// 静音
 	jingyin.onclick = function () {
 		if (!audio.muted) {
@@ -142,27 +144,25 @@ window.onload = function () {
 	// 判断文件缓冲进度
 	setInterval(function updateCache () {
 		var buffered, percent;
-		console.log('执行缓冲');
 		// 已缓冲部分
 		audio.readyState == 4 && (buffered = audio.buffered.end(0));
 
 		// 已缓冲百分百
 		audio.readyState == 4 && (percent = Math.round(buffered / audio.duration * 100) + '%');
 		loadBar.style.width = (Math.round(buffered / audio.duration * 100) * musicBar.clientWidth * 0.01) + 'px';
-		console.log(Math.round(buffered / audio.duration * 100));
-		console.log(percent);
 	}, 1000);
 
 	// 计算音乐时间
 
 	function musicTime() {
+		// 更换播放歌曲
+		musicTitle.innerHTML = sourceList[currentSrcIndex].title;
 		// 播放时间显示
 		audio.addEventListener("canplay", function(){
 			var sc=audio.duration;
 
 			var fen = parseInt(sc / 60);
 			var miao = parseInt(sc % 60);
-			console.log('秒为====' + miao);
 			if (miao < 10) {
 				totalTime.innerHTML = fen + ':0' + miao;
 			}else {
